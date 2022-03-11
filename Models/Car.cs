c176d88398fa0769674694d99a46046a1b919d6f
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-03-11 17:08:39 +0000

    Initial commit

---
+++ Models/Car.cs
@@ -0,0 +1,102 @@
+﻿namespace GeneticCars.Models;
+
+using System.Drawing;
+using Excubo.Blazor.Canvas.Contexts;
+
+public sealed class Car : IDrawable
+{
+  public const int Width = 20;
+  public const int Height = 20;
+  public const int LidarSenseDist = 100;
+
+  private Point _position = new(20, 20);
+  private double _heading; // degrees
+
+  public async Task Draw(Context2D ctx)
+  {
+    await ctx.SaveAsync();
+    try
+    {
+      await ctx.TranslateAsync(_position.X, _position.Y);
+      await ctx.RotateAsync(_heading.ToRadians());
+      await ctx.TranslateAsync(-_position.X, -_position.Y);
+
+      // body
+      await ctx.FillStyleAsync("blue");
+      await ctx.FillRectAsync(_position.X, _position.Y, Width, Height);
+
+      // nose
+      const int NoseWidth = 10;
+      const int NoseHeight = 10;
+      await ctx.FillStyleAsync("green");
+      await ctx.FillRectAsync(_position.X + Width / 2 - NoseWidth / 2, _position.Y - 3, NoseWidth, NoseHeight);
+
+      // lidar
+      const int LidarSenseWidth = 3;
+      await ctx.StrokeStyleAsync("yellow");
+      await ctx.LineWidthAsync(LidarSenseWidth);
+      await ctx.BeginPathAsync();
+
+      var nose = new Point(_position.X + Width / 2, _position.Y);
+
+      // forward
+      await ctx.MoveToAsync(nose.X, nose.Y);
+      await ctx.LineToAsync(nose.X, _position.Y - LidarSenseDist);
+
+      // right
+      await ctx.MoveToAsync(nose.X, nose.Y);
+      await ctx.LineToAsync(nose.X + LidarSenseDist, nose.Y);
+
+      // left
+      await ctx.MoveToAsync(nose.X, nose.Y);
+      await ctx.LineToAsync(nose.X - LidarSenseDist, nose.Y);
+
+      // forward-right
+      await ctx.MoveToAsync(nose.X, nose.Y);
+      await ctx.LineToAsync(nose.X + LidarSenseDist * 0.707, nose.Y - LidarSenseDist * 0.707);
+
+      // forward-left
+      await ctx.MoveToAsync(nose.X, nose.Y);
+      await ctx.LineToAsync(nose.X - LidarSenseDist * 0.707, nose.Y - LidarSenseDist * 0.707);
+
+      // back
+      await ctx.MoveToAsync(nose.X, nose.Y + Height);
+      await ctx.LineToAsync(nose.X, nose.Y + Height + LidarSenseDist);
+
+      await ctx.StrokeAsync();
+    }
+    finally
+    {
+      await ctx.RestoreAsync();
+    }
+  }
+
+  public void Move(int deltaX, int deltaY)
+  {
+    _position.X += deltaX;
+    _position.Y += deltaY;
+  }
+
+  public void Rotate(double degrees)
+  {
+    _heading += degrees;
+  }
+}
+
+public interface IDrawable
+{
+  Task Draw(Context2D ctx);
+}
+
+/// <summary>
+/// Convert to Radians.
+/// </summary>
+/// <param name="val">The value to convert to radians</param>
+/// <returns>The value in radians</returns>
+public static class NumericExtensions
+{
+  public static double ToRadians(this double val)
+  {
+    return (Math.PI / 180) * val;
+  }
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace GeneticCars.Pages;
+
+using System.Timers;
+using Excubo.Blazor.Canvas;
+using Models;
+using Timer = System.Timers.Timer;
+
+public partial class Index
+{
+  private const int CanvasWidth = 800;
+  private const int CanvasHeight = 800;
+  
+  private Canvas _canvas;
+  private int _count;
+  private readonly Timer _timer = new(1000);
+
+  private List<IDrawable> _drawables = new();
+
+  public Index()
+  {
+    var car1 = new Car();
+    _drawables.Add(car1);
+    
+    _timer.Elapsed += TimerOnElapsed;
+  }
+
+  private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+  {
+    _count++;
+    var car = _drawables.OfType<Car>().SingleOrDefault();
+    car?.Move(10, 10);
+    car?.Rotate(15);
+    InvokeAsync(StateHasChanged);
+  }
+
+  protected override async Task OnAfterRenderAsync(bool firstRender)
+  {
+    await using var ctx = await _canvas.GetContext2DAsync();
+    await ctx.ClearRectAsync(0, 0, CanvasWidth, CanvasHeight);
+    await ctx.FontAsync("48px solid");
+    await ctx.FillTextAsync(_count.ToString(), 0, 150);
+    var draws = _drawables.Select(d => d.Draw(ctx));
+    await Task.WhenAll(draws);
+  }
+
+  private void OnStartClick()
+  {
+    _timer.Enabled = !_timer.Enabled;
+  }
+}
