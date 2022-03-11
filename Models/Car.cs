b59088494f7330fa912e2a85f8be0d7b72ea2dc7
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-03-11 17:09:43 +0000

    - refactored

--- Models/Car.cs
+++ Models/Car.cs
@@ -82,21 +82,3 @@ public sealed class Car : IDrawable
     _heading += degrees;
   }
 }
-
-public interface IDrawable
-{
-  Task Draw(Context2D ctx);
-}
-
-/// <summary>
-/// Convert to Radians.
-/// </summary>
-/// <param name="val">The value to convert to radians</param>
-/// <returns>The value in radians</returns>
-public static class NumericExtensions
-{
-  public static double ToRadians(this double val)
-  {
-    return (Math.PI / 180) * val;
-  }
-}
---
+++ Models/IDrawable.cs
@@ -0,0 +1,8 @@
+﻿using Excubo.Blazor.Canvas.Contexts;
+
+namespace GeneticCars.Models;
+
+public interface IDrawable
+{
+  Task Draw(Context2D ctx);
+}
