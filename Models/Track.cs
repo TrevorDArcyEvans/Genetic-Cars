c9a5413b051a6722542322f6cb4b6f2f217b9ede
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-03-16 08:20:25 +0000

    - refactored

--- Models/Track.cs
+++ Models/Track.cs
@@ -15,7 +15,7 @@ public sealed class Track
 
   public int Width => _data.GetLength(0);
   public int Height => _data.GetLength(1);
-  public double Direction { get; private set; } // degrees
+  public double Direction { get; } // degrees
 
   // width x height
   // [x, y] track points
